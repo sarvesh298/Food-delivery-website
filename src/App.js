7a3448c1b61777f4fadef8954fc3b859cedd09cc
Jayavaram Ranga Sarvesh <rangasarvesh.23bce9681@vitapstudent.ac.in>
2025-06-08 14:38:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route path="/menu" element={<Menu />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
         <Footer />
       </Router>
